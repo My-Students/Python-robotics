7b70f5dd1a62ef6f1ad305a3bd096b6eb1f87bd5
Badoino Matteo <48619446+BadoinoMatteo@users.noreply.github.com>
2020-03-30 17:31:45 +0200

    Add files via upload

--- Robot_e grafi_solutions/es_RobotBadoino.py
+++ Robot_e grafi_solutions/es_RobotBadoino.py
@@ -11,31 +11,49 @@ def dimPavimento():
 def formazionePavimento(piastrelle):
     mat = []
     for a in range(0, piastrelle):
+        riga=[]
         for b in range(0, piastrelle):
             num = int(input(f"inserisci se la cella {a} {b} è libera o occupata :\n 0 --> cella libera \n 1 --> cella occupata \n "))
             if num == 0:
-                mat.append(True)
+                riga.append(True)
             else:
-                mat.append(False)
-
-
+                riga.append(False)
+        mat.append(riga)
+    creaNodi(mat)
+    print(mat)
     return mat
+
+def creaNodi(pav):
+    nNodi = 1
+    for r in range(0, len(pav)):
+        for c in range(0, len(pav)):
+            if pav[r][c]:
+                pav[r][c] = nNodi
+                nNodi += 1
+
+
+
 
 def costruiscoMatrice (piastrelle , matBool):
     dict = {}
     numNodi = 1
+    offset=1
     for a in range(0, piastrelle):
         for b in range(0, piastrelle):
             collegamenti = []
             if matBool[a][b] != False :
-                if a-1 >= 0:
-                    if matBool[a-1][b] != False: collegamenti.append(matBool[a-1][b])
-                if b-1 >= 0:
-                    if matBool[a][b-1]!=False:collegamenti.append(matBool[a][b-1])
-                if a+1 >= 0:
-                    if matBool[a+1][b] != False: collegamenti.append(matBool[a+1][b])
-                if b+1 >= 0:
-                    if matBool[a][b+1]!=False:collegamenti.append(matBool[a][b+1])
+                if a-offset >= 0:
+                    if matBool[a-offset][b] != False:
+                        collegamenti.append(matBool[a-1][b])
+                if b-offset >= 0:
+                    if matBool[a][b-offset]!=False:
+                        collegamenti.append(matBool[a][b-1])
+                if a+offset < len(matBool) :
+                    if matBool[a+offset][b] != False:
+                        collegamenti.append(matBool[a+1][b])
+                if b+offset < len(matBool) :
+                    if matBool[a][b+offset]!=False:
+                        collegamenti.append(matBool[a][b+1])
             dict[numNodi] = collegamenti
             numNodi += 1
     print(dict)
